777adc23ce63b197124e5f6fa89ebb40c18634e9
supplient <437566830@qq.com>
2022-06-09 16:37:32 +0800

    调试

--- work.sh
+++ work.sh
@@ -8,6 +8,14 @@ echo "css_file: ${css_file}"
 echo "md_dir: ${md_dir}"
 echo "html_dir: ${html_dir}"
 
+# ???
+echo "[Debug Start]"
+echo "ls ${md_dir}"
+ls ${md_dir}
+echo "ls /home/runner/work/GithubActionTest/GithubActionTest"
+ls /home/runner/work/GithubActionTest/GithubActionTest
+echo "[Debug End]"
+
 # 创建输出目录以免不存在，-R是为了在目录已存在时也不会报错
 mkdir -p $html_dir
 
